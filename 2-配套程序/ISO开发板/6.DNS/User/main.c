f578210f3fa2f92556bee29304f16fe45b132f68
flyleaf <flyleaf91@163.com>
2019-03-12 12:05:41 +0800

    迁移旧资料

---
+++ 2-配套程序/ISO开发板/6.DNS/User/main.c
@@ -0,0 +1,65 @@
+/**
+******************************************************************************
+* @file    			main.c
+* @author  			WIZnet Software Team
+* @version 			V1.0
+* @date    			2015-02-14
+* @brief   			用3.5.0版本库建的工程模板
+* @attention  	实验平台：野火 iSO-MINI STM32 开发板 + 野火W5500网络适配板
+*
+*               默认使用野火开发板的SPI1接口，请断开“J10”A4与cs间的短接帽
+
+*								本测试为外网测试，请保证W5500的网线可以访问Internet
+* 							可以自动获取IP地址，也可以使用默认指定的IP地址
+*
+* 实验平台:野火 iSO-MINI STM32 开发板 
+* 论坛    :http://www.chuxue123.com
+* 淘宝    :http://firestm32.taobao.com
+*
+******************************************************************************
+*/ 
+
+#include <stdio.h>
+#include <string.h>
+
+#include "stm32f10x.h"
+
+#include "bsp_usart1.h"
+#include "bsp_i2c_ee.h"
+#include "bsp_i2c_gpio.h"
+#include "bsp_led.h"
+
+#include "w5500.h"
+#include "W5500_conf.h"
+#include "socket.h"
+#include "utility.h"
+
+/*app函数头文件*/
+#include "dns.h"
+
+int main(void)
+{ 
+	systick_init(72);										/*初始化Systick工作时钟*/
+	USART1_Config();										/*初始化串口通信:115200@8-n-1*/
+	i2c_CfgGpio();											/*初始化eeprom*/
+
+  printf("  野火网络适配版 DNS Demo V1.0 \r\n");	
+  
+	gpio_for_w5500_config();						/*初始化MCU相关引脚*/
+	reset_w5500();											/*硬复位W5500*/
+	set_w5500_mac();										/*配置MAC地址*/
+  set_w5500_ip();											/*配置IP地址*/
+	
+  socket_buf_init(txsize, rxsize);		/*初始化8个Socket的发送接收缓存大小*/
+
+	printf(" 解析%s的IP\r\n",domain_name); 
+	
+	while(1)														/*循环执行的函数*/ 
+	{		
+		do_dns();													/*域名解析测试程序*/
+		delay_ms(1000);
+	}
+} 
+
+
+
